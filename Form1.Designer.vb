ff58dcddca732fa8d9d730cb1bf80fde40838a2f
KehetsikOganovna <39780551+KehetsikOganovna@users.noreply.github.com>
2018-05-31 01:18:41 +0300

    VB Array

---
+++ Form1.Designer.vb
@@ -0,0 +1,354 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Форма переопределяет dispose для очистки списка компонентов.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Является обязательной для конструктора форм Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'Примечание: следующая процедура является обязательной для конструктора форм Windows Forms
+    'Для ее изменения используйте конструктор форм Windows Form.  
+    'Не изменяйте ее в редакторе исходного кода.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton3 = New System.Windows.Forms.RadioButton()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.RadioButton4 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton5 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton6 = New System.Windows.Forms.RadioButton()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.TextBox7 = New System.Windows.Forms.TextBox()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Panel1.SuspendLayout()
+        Me.Panel2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(33, 27)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(134, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Размерность массива 1:"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(170, 24)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(53, 20)
+        Me.TextBox1.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(258, 27)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(134, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Размерность массива 2:"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(398, 24)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(55, 20)
+        Me.TextBox2.TabIndex = 3
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(524, 18)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(199, 30)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "Сформировать массивы"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(33, 69)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(91, 13)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Первый массив:"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(36, 95)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(687, 20)
+        Me.TextBox3.TabIndex = 6
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(33, 131)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(87, 13)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Второй массив:"
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(36, 160)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(687, 20)
+        Me.TextBox4.TabIndex = 8
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(524, 189)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(199, 31)
+        Me.Button2.TabIndex = 9
+        Me.Button2.Text = "Объединить"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(33, 207)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(128, 13)
+        Me.Label5.TabIndex = 10
+        Me.Label5.Text = "Объединенный массив:"
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(36, 235)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(687, 20)
+        Me.TextBox5.TabIndex = 11
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.RadioButton3)
+        Me.Panel1.Controls.Add(Me.RadioButton2)
+        Me.Panel1.Controls.Add(Me.RadioButton1)
+        Me.Panel1.Location = New System.Drawing.Point(36, 277)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(154, 127)
+        Me.Panel1.TabIndex = 12
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(71, 277)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(90, 13)
+        Me.Label6.TabIndex = 13
+        Me.Label6.Text = "Выбор массива:"
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Checked = True
+        Me.RadioButton1.Location = New System.Drawing.Point(35, 30)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(65, 17)
+        Me.RadioButton1.TabIndex = 14
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "Первый"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Location = New System.Drawing.Point(35, 53)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(61, 17)
+        Me.RadioButton2.TabIndex = 14
+        Me.RadioButton2.TabStop = True
+        Me.RadioButton2.Text = "Второй"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'RadioButton3
+        '
+        Me.RadioButton3.AutoSize = True
+        Me.RadioButton3.Location = New System.Drawing.Point(35, 76)
+        Me.RadioButton3.Name = "RadioButton3"
+        Me.RadioButton3.Size = New System.Drawing.Size(102, 17)
+        Me.RadioButton3.TabIndex = 14
+        Me.RadioButton3.TabStop = True
+        Me.RadioButton3.Text = "Объединённый"
+        Me.RadioButton3.UseVisualStyleBackColor = True
+        '
+        'Panel2
+        '
+        Me.Panel2.Controls.Add(Me.TextBox6)
+        Me.Panel2.Controls.Add(Me.RadioButton6)
+        Me.Panel2.Controls.Add(Me.RadioButton5)
+        Me.Panel2.Controls.Add(Me.RadioButton4)
+        Me.Panel2.Location = New System.Drawing.Point(213, 277)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(510, 161)
+        Me.Panel2.TabIndex = 14
+        '
+        'RadioButton4
+        '
+        Me.RadioButton4.AutoSize = True
+        Me.RadioButton4.Checked = True
+        Me.RadioButton4.Location = New System.Drawing.Point(12, 21)
+        Me.RadioButton4.Name = "RadioButton4"
+        Me.RadioButton4.Size = New System.Drawing.Size(14, 13)
+        Me.RadioButton4.TabIndex = 0
+        Me.RadioButton4.TabStop = True
+        Me.RadioButton4.UseVisualStyleBackColor = True
+        '
+        'RadioButton5
+        '
+        Me.RadioButton5.AutoSize = True
+        Me.RadioButton5.Location = New System.Drawing.Point(12, 67)
+        Me.RadioButton5.Name = "RadioButton5"
+        Me.RadioButton5.Size = New System.Drawing.Size(14, 13)
+        Me.RadioButton5.TabIndex = 1
+        Me.RadioButton5.TabStop = True
+        Me.RadioButton5.UseVisualStyleBackColor = True
+        '
+        'RadioButton6
+        '
+        Me.RadioButton6.AutoSize = True
+        Me.RadioButton6.Location = New System.Drawing.Point(12, 114)
+        Me.RadioButton6.Name = "RadioButton6"
+        Me.RadioButton6.Size = New System.Drawing.Size(14, 13)
+        Me.RadioButton6.TabIndex = 2
+        Me.RadioButton6.TabStop = True
+        Me.RadioButton6.UseVisualStyleBackColor = True
+        '
+        'TextBox6
+        '
+        Me.TextBox6.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.TextBox6.Location = New System.Drawing.Point(32, 3)
+        Me.TextBox6.Multiline = True
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.Size = New System.Drawing.Size(475, 155)
+        Me.TextBox6.TabIndex = 3
+        Me.TextBox6.Text = resources.GetString("TextBox6.Text")
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(36, 444)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(687, 33)
+        Me.Button3.TabIndex = 15
+        Me.Button3.Text = "Решение"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(33, 495)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(62, 13)
+        Me.Label7.TabIndex = 16
+        Me.Label7.Text = "Результат:"
+        '
+        'TextBox7
+        '
+        Me.TextBox7.Location = New System.Drawing.Point(36, 519)
+        Me.TextBox7.Name = "TextBox7"
+        Me.TextBox7.Size = New System.Drawing.Size(684, 20)
+        Me.TextBox7.TabIndex = 17
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(556, 553)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(164, 34)
+        Me.Button4.TabIndex = 18
+        Me.Button4.Text = "Выход"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(744, 599)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.TextBox7)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Одномерные массивы"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.Panel2.ResumeLayout(False)
+        Me.Panel2.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label5 As Label
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Label6 As Label
+    Friend WithEvents RadioButton3 As RadioButton
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents RadioButton6 As RadioButton
+    Friend WithEvents RadioButton5 As RadioButton
+    Friend WithEvents RadioButton4 As RadioButton
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Label7 As Label
+    Friend WithEvents TextBox7 As TextBox
+    Friend WithEvents Button4 As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,198 @@
+﻿Public Class Form1
+
+    'Описание динамических массивов
+    Dim a() As Integer
+    Dim b() As Integer
+    Dim c() As Integer
+    Dim n, m As Integer
+
+    Private Sub Label5_Click(sender As Object, e As EventArgs) Handles Label5.Click
+
+    End Sub
+
+    Private Sub Label6_Click(sender As Object, e As EventArgs) Handles Label6.Click
+
+    End Sub
+
+    Private Sub TextBox6_TextChanged(sender As Object, e As EventArgs) Handles TextBox6.TextChanged
+
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Me.Close() ' Кнопка "Выход"
+    End Sub
+
+    'Формирование массива и заполнение случайными числами
+    Private Sub RandomArr(n As Integer, ByRef a() As Integer)
+        Dim i, x, y As Integer
+        'Нижняя граница интервала возможных значений элементов массива
+        x = -10 * n
+        'Верхняя граница интервала возможных значений элементов массива
+        y = 10 * n
+        For i = 0 To n - 1
+            a(i) = CInt((y - x) * Rnd()) + x
+        Next i
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        TextBox3.Text = ""
+        TextBox4.Text = ""
+        TextBox5.Text = ""
+        n = Val(TextBox1.Text) 'размер первого массива 
+        m = Val(TextBox2.Text) 'размер второго массива 
+        'определим размеры наших динамических массивов
+        ReDim a(n - 1)
+        ReDim b(m - 1)
+        'Заполним первый массив случайными числами
+        RandomArr(n, a)
+        'Вывод второго массива
+        For i = 0 To n - 1
+            TextBox3.Text = TextBox3.Text & Str(a(i)) & " "
+        Next i
+        'Заполним второй массив случайными числами
+        RandomArr(m, b)
+        'Вывод второго массива
+        For i = 0 To m - 1
+            TextBox4.Text = TextBox4.Text & Str(b(i)) & " "
+        Next i
+    End Sub
+
+    Private Sub TextBox3_TextChanged(sender As Object, e As EventArgs) Handles TextBox3.TextChanged
+
+    End Sub
+
+    Private Sub TextBox4_TextChanged(sender As Object, e As EventArgs) Handles TextBox4.TextChanged
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        'Размер объединенного массива n + m
+        ReDim c(n + m)
+        'В начале массива с() сначала следуют элементы массива а()
+        For i = 0 To n - 1
+            c(i) = a(i)
+        Next i
+        ' А затем  массива b()
+        For i = n To (n + m) - 1
+            c(i) = b(i - n)
+        Next i
+        'Вывод на TextBox
+        For i = 0 To (n + m) - 1
+            TextBox5.Text = TextBox5.Text & Str(c(i)) & " "
+        Next i
+    End Sub
+
+    'Решение Задачи1: "1. Найти сумму положительных элементов, значения которых,  состоят из двух цифр."
+    Private Function SumPositDouble(k As Integer, a() As Integer) As Integer
+        SumPositDouble = 0
+        For i = 0 To k - 1
+            'Если число положительное и двузначное, то добавить к сумме
+            If (a(i) > 0) And (a(i) > 10) And (a(i) < 100) Then
+                SumPositDouble = SumPositDouble + a(i)
+            End If
+        Next i
+    End Function
+
+    'Число v кратно k
+    Private Function Multiple(v As Integer, k As Integer) As Boolean
+        ' Если полное частное минус целочисленное частное = 0, то делит нацело
+        Multiple = (v / k - v \ k = 0)
+    End Function
+
+    'Решение Задачи2: "2. Найти количество тех элементов, значения которых по модулю превосходят 10 и кратны 5 и 10."
+    Private Function CountMore10(k As Integer, a() As Integer) As Integer
+        CountMore10 = 0
+        For i = 0 To k - 1
+            'Если модуль числа больше 10 и кратно 5 и 10, то увеличить счётчик на 1
+            If (Math.Abs(a(i)) > 10) And Multiple(a(i), 5) And Multiple(a(i), 10) Then
+                CountMore10 = CountMore10 + 1
+            End If
+        Next i
+    End Function
+
+    Function Swap(ByRef x As Integer, ByRef y As Integer)
+        Dim v As Integer
+        v = x
+        x = y
+        y = v
+    End Function
+
+    'Решение Задачи3: "3. Поменять местами 2, 4 и 6 элемент с тремя последними элементами, сохраняя порядок их следования."
+    Private Function Swap246(k As Integer, ByRef a() As Integer) As Integer()
+        If k < 6 Then
+            Exit Function
+        End If
+        Swap(a(1), a(k - 3)) 'второй элемент это a(1), т.к. отсчёт идёт с 0
+        Swap(a(3), a(k - 2)) 'предпоследний элемент это a(k - 2), т.к. последний элемент это a(k - 1)
+        Swap(a(5), a(k - 1)) 'последний элемент это a(k - 1), т.к. отсчёт идёт с 0
+        Swap246 = a
+    End Function
+
+    'Печать массива
+    Private Function ShowArr(k As Integer, a() As Integer) As String
+        If k < 6 Then
+            ShowArr = "Ошибка: массив должен быть размером больше 6"
+            Exit Function
+        End If
+        ShowArr = ""
+        For i = 0 To k - 1
+            ShowArr = ShowArr & a(i) & " "
+        Next i
+    End Function
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        'Если выбрана первая задача (четвертая кнопка)
+        If RadioButton4.Checked() Then
+            'Если нажата первая кнопка (выбран для работы первый массив)
+            If RadioButton1.Checked() Then
+                TextBox7.Text = SumPositDouble(n, a)
+            End If
+            'Если нажата вторая кнопка (выбран для работы второй массив)
+            If RadioButton2.Checked() Then
+                TextBox7.Text = SumPositDouble(m, b)
+            End If
+            'Если нажата третья кнопка (выбран для работы третий массив)
+            If RadioButton3.Checked() Then
+                TextBox7.Text = SumPositDouble(n + m, c)
+            End If
+        End If
+
+
+        'Если выбрана вторая задача (пятая кнопка)
+        If RadioButton5.Checked() Then
+            'Если нажата первая кнопка (выбран для работы первый массив)
+            If RadioButton1.Checked() Then
+                TextBox7.Text = CountMore10(n, a)
+            End If
+            'Если нажата первая кнопка (выбран для работы первый массив)
+            If RadioButton2.Checked() Then
+                TextBox7.Text = CountMore10(m, b)
+            End If
+            'Если нажата первая кнопка (выбран для работы первый массив)
+            If RadioButton3.Checked() Then
+                TextBox7.Text = CountMore10(n + m, c)
+            End If
+        End If
+
+
+        'Если выбрана третья задача (шестая кнопка)
+        If RadioButton6.Checked() Then
+            'Если нажата первая кнопка (выбран для работы первый массив)
+            If RadioButton1.Checked() Then
+                TextBox7.Text = ShowArr(n, Swap246(n, a))
+            End If
+            'Если нажата первая кнопка (выбран для работы первый массив)
+            If RadioButton2.Checked() Then
+                TextBox7.Text = ShowArr(m, Swap246(m, b))
+            End If
+            'Если нажата первая кнопка (выбран для работы первый массив)
+            If RadioButton3.Checked() Then
+                TextBox7.Text = ShowArr(n + m, Swap246(n + m, c))
+            End If
+        End If
+    End Sub
+
+    Private Sub TextBox5_TextChanged(sender As Object, e As EventArgs) Handles TextBox5.TextChanged
+
+    End Sub
+End Class
